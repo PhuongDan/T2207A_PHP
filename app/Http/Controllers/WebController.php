0e3a082d8f7c5dbc9ba26cb369e716953ed31ce0
PhuongDan <110390687+PhuongDan@users.noreply.github.com>
2023-05-15 14:56:27 +0700

    up

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function home(){
-        return view("welcome");
+
+        //lay san pham trong db
+        $new_products = Products::orderBy("id","desc")->limit(6)->get();
+        $new_categories = Category::limit(10)->get();
+        $new_productss =Products::limit(12)->get();
+        return view("welcome",
+        ["new_products"=>$new_products,
+            "categories"=>$new_categories,
+            "new_productss"=>$new_productss
+        ]);
+
+
+
     }
     public function shop(){
         return view("shop");
